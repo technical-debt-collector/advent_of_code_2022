efb1695df192609a8a482ef1af0418921752a83a
Ola Sæter Heitmann <ola.saeter.heitmann@gmail.com>
2022-12-07 00:16:31 +0100

    Day 6 part 2 lol

--- src/main/java/puzzle/Day6Part2.java
+++ src/main/java/puzzle/Day6Part2.java
@@ -1,26 +1,23 @@
 package puzzle;
 
 import java.io.IOException;
-import java.util.*;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
+import java.util.LinkedList;
 
 
-public class Day6Part1 extends Puzzle {
+public class Day6Part2 extends Puzzle {
 
     public static void main(String[] args) throws IOException {
         var i = 0;
         int dupes = 0;
         var lhs = new LinkedList<>();
         for (char c : input("day6.txt").toCharArray()) {
-            if (++i > 4 && lhs.contains(lhs.removeFirst()))
+            if (++i > 14 && lhs.contains(lhs.removeFirst()))
                 dupes--;
             if (lhs.contains(c))
                 dupes++;
             lhs.add(c);
 
-            if (lhs.size() == 4 && dupes == 0)
+            if (lhs.size() == 14 && dupes == 0)
                 break;
         }
         System.out.println(i);

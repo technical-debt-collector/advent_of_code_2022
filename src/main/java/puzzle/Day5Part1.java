795a644836017347dedbe829f1549545a7a5585f
Ola Sæter Heitmann <ola.saeter.heitmann@gmail.com>
2022-12-06 20:32:01 +0100

    Day 5 part one done

--- src/main/java/puzzle/Day5Part1.java
+++ src/main/java/puzzle/Day5Part1.java
@@ -28,18 +28,19 @@ public class Day5Part1 extends Puzzle {
             index++;
         }
 
-
-        String[] split = instructions.split("\\n");
-        for (int j = 1; j < split.length; j++) {
-            String instruction = split[j];
-            var pattern = Pattern.compile("\\d+");
+        var pattern = Pattern.compile("\\d+");
+        for (String instruction : instructions.split("\\n")) {
             var matcher = pattern.matcher(instruction);
-
             matcher.find();
-            System.out.println(matcher.results().toArray().length);
-            for (int i = 0; i < Integer.parseInt(matcher.group(0)); i++) {
-                var item = map.get(Integer.parseInt(matcher.group(1))).pop();
-                map.get(Integer.parseInt(matcher.group(2))).push(item);
+            var iterations = Integer.parseInt(matcher.group(0));
+            matcher.find();
+            var from = Integer.parseInt(matcher.group(0));
+            matcher.find();
+            var to = Integer.parseInt(matcher.group(0));
+
+            for (int i = 0; i < iterations; i++) {
+                var item = map.get(from).pop();
+                map.get(to).push(item);
             }
         }
         System.out.println(map.values().stream().map(Vector::lastElement).collect(Collectors.toList()));
